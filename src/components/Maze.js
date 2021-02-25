bc2b941f2cfb15e84339e8dfd0e04cb99c68cb3e
SushantAA <sushant.co19@gmail.com>
2021-02-25 06:55:09 +0530

    add : component setup

---
+++ src/components/Maze.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Box from './Box'
+
+export default function Maze() {
+    return (
+        <div>
+            <h1>Maze</h1>
+            <Box/>
+        </div>
+    )
+}
